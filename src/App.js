6068b9364017d7d4c14eff2d3cecc9684231e14a
nosmith18 <nosmith@wisc.edu>
2023-02-04 02:18:34 -0600

    First dev commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>First things first... here's a Header!</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
